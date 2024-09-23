8c301f5e9083e3fa13247966135f14e44de55c67
Mili-Akther <miliakthermilu@gmail.com>
2024-09-23 12:12:35 +0600

    first commit

---
+++ JS/login.js
@@ -0,0 +1,7 @@
+// step-1 set eve handler
+document.getElementById("button-login").addEventListener("click", function () {
+  console.log("login button clicked");
+  //   step: 3 - get the phone number
+  const phoneNumber = document.getElementById("phone-number").value;
+  console.log(phoneNumber);
+});
