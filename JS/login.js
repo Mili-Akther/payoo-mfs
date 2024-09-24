8b429a229a23bcf2a29e7b71fb4dd490efaade82
Mili-Akther <miliakthermilu@gmail.com>
2024-09-24 17:49:23 +0600

    added home page designs

--- JS/login.js
+++ JS/login.js
@@ -1,7 +1,19 @@
 // step-1 set eve handler
 document.getElementById("button-login").addEventListener("click", function () {
-  console.log("login button clicked");
-  //   step: 3 - get the phone number
+  //   step: 3 - get the phone number and pin number
   const phoneNumber = document.getElementById("phone-number").value;
-  console.log(phoneNumber);
+  const pinNumber = document.getElementById("pin-number").value;
+
+  console.log(phoneNumber, pinNumber);
+
+  //   step-4: validate phone and pin-->
+  // This is temporary
+  if (phoneNumber === "5" && pinNumber === "1234") {
+    console.log("you are logged in");
+    window.location.href = "/home.html";
+  }
+  //   step-5: allow user to use the website
+  else {
+    alert("wrong phone or pin");
+  }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.html}"],
   theme: {
     extend: {},
   },
